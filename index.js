8c8f7b61f5fe4d7eb613dfa05341ddbfe8de0dec
ikp-773 <abhijithkp773@gmail.com>
2020-09-03 20:26:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ for (var i = 0; i < drumButton; i++) {
   document.querySelectorAll(".drum")[i].addEventListener('click', handleClick);
 }
 
+
+
+
+
 function handleClick() {
-  var audio =new Audio('sounds/snare.mp3');
-  audio.play();
+
 }
+
+
+
+
+
+// var audio =new Audio('sounds/snare.mp3');
+// audio.play();
