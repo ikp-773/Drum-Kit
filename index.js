d6b6533540193058585b639fdc1fa1b0131a5a78
ikp-773 <abhijithkp773@gmail.com>
2020-09-03 18:30:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 
 var drumButton=document.querySelectorAll(".drum").length;
-if(var i=0;i<drumButton;i++){
+
+
+for(var i=0;i<drumButton;i++){
   document.querySelectorAll(".drum")[i].addEventListener('click',handleClick);
 }
-
-
 
 function handleClick() {
   alert('i got clicked');
