b6120c26469111c9a41bee3b22559bfcd8b65d77
ikp-773 <abhijithkp773@gmail.com>
2020-09-04 15:38:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ for (var i = 0; i < drumButton; i++) {
 
 
 function handleClick() {
-  var key = this.innerHTMML;
+  var key = this.innerHTML;
   switch (key) {
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case 'w':
+      var tom3 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
     case 'w':
       var tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
