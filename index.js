8f47f820e2abbde5c70a35d2d7f97ea509a7f85c
ikp-773 <abhijithkp773@gmail.com>
2020-09-04 15:39:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,19 +16,18 @@ function handleClick() {
       var tom2 = new Audio('sounds/tom-2.mp3');
       tom2.play();
       break;
-    case 'w':
-      var tom3 = new Audio('sounds/tom-1.mp3');
-      tom1.play();
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
       break;
-    case 'w':
+    case 'j':
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case 'k':
       var tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
       break;
-    case 'w':
-      var tom1 = new Audio('sounds/tom-1.mp3');
-      tom1.play();
-      break;
-    default:
-
+    default:console.log(key);
   }
 }
